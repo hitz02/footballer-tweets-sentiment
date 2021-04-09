de17dac117b69aaa87001c8a675844234329c7d1
hitz02 <hiteshw.02@gmail.com>
2021-04-09 16:14:02 +0530

    change db param in settings

--- foot_tweet/settings.py
+++ foot_tweet/settings.py
@@ -78,9 +78,15 @@ WSGI_APPLICATION = 'foot_tweet.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 db_from_env = dj_database_url.config(conn_max_age=600)
-DATABASES = {}
+
 DATABASES['default'].update(db_from_env)
 
 
